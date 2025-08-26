d21076fbe6280fabbbc1e85108504b31ee205b63
aajuvale <ahmed.juvale@gmail.com>
2025-08-25 17:22:31 -0700

    feat: implemented current month and back and forward buttons

--- Calendrical/ContentView.swift
+++ Calendrical/ContentView.swift
@@ -8,15 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State
+    private var monthString = ""
+    @State
+    private var count = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(alignment: .top) {
+                if let monthInt = Calendar.current.dateComponents([.month], from: Date()).month {
+                    var monthStr = Calendar.current.monthSymbols[monthInt - 1]
+                    Text(monthStr)
+                        .font(.title)
+
+                    Spacer()
+
+                    Button {
+                        // Back arrow takes to previous month
+//                        count -= 1
+//                        monthString = Calendar.current.monthSymbols[monthInt-count-1]
+                    } label: {
+                        Image(systemName: "chevron.backward")
+                    }
+                    .frame(alignment: .trailing)
+
+                    Button {
+                        // Forward arrow takes to next month
+                    } label: {
+                        Image(systemName: "chevron.forward")
+                    }
+                    .frame(alignment: .trailing)
+                }
+            }
+
+//            GridRow {
+//            }
         }
+        .frame(width: 500, height: 1000)
         .padding()
     }
+
+    private func getCurrentMonth(monthInt: Int) -> String {
+        Calendar.current.monthSymbols[monthInt - 1]
+    }
+
 }
 
 #Preview {
